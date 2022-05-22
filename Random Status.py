f529337188eb1ed89562a97aacf8b72058dc5b68
Ilham Apriansyah <ilhamapriansyah471@gmail.com>
2022-05-22 19:45:52 +0700

    penambahan API untuk Bot Telegram

--- Random Status.py
+++ Random Status.py
@@ -1,24 +1,50 @@
+from telegram.ext.updater import Updater
+from telegram.update import Update
+from telegram.ext.callbackcontext import CallbackContext
+from telegram.ext.commandhandler import CommandHandler
+from telegram.ext.messagehandler import MessageHandler
+from telegram.ext.filters import Filters
 import random
 
 Status		= ['Jomblo','Menikah','Pacaran']
-Greeting	= ['Selamat Pagi','Selamat Siang','Selamat Malam']
+Greeting	= ['Selamat Pagi','Selamat Siang','Selamat Sore','Selamat Malam']
 Gender		= ['Laki-laki','Perempuan']
 Job			= ['Sekolah','Guru anak tk','Pengacara','Tidak Ada']
-Hobby		= ['Turu','Main game','Ngoprek']
+Hobby		= ['Turu','Main game','Ngoprek','Belajar']
 Quotes		= ['Jangan semangat!','Teruslah gagal!','Teruslah hidup!','Semangat!']
 
-#output
-def output():
-	print('\n====== ' +	random.choice(Greeting) )
+updater = Updater("#Gunakan API Bot Telegram",
+                  use_context=True)
 
-	print('\n====== hai, statusku saat ini ' +	random.choice(Status) )
+def mulai(update: Update, context: CallbackContext):
+    update.message.reply_text("selamat datang di Random Status!! \nsilahkan dilihat /bantuan untuk menjalankan bot ini")
 
-	print('\n====== jenis kelaminku adalah ' +	random.choice(Gender) )
+def bantuan(update: Update, context: CallbackContext):
+	update.message.reply_text("klik /jalankan dan bot secara otomatis akan berjalan")
 
-	print('\n====== dan aku punya hobby ' +	random.choice(Hobby) )
+def unknown(update: Update, context: CallbackContext):
+    update.message.reply_text("Maaf, perintah '%s' tidak valid!!" % update.message.text)
+
+def bot(update: Update, context: CallbackContext):
+	update.message.reply_text(random.choice(Greeting) )
+
+	update.message.reply_text(' statusku saat ini ' +	random.choice(Status) )
+
+	update.message.reply_text(' jenis kelaminku adalah ' +	random.choice(Gender) )
+
+	update.message.reply_text(' dan aku punya hobby ' +	random.choice(Hobby) )
 	
-	print('\n====== untuk saat ini, pekerjaanku ' +	random.choice(Job) )
+	update.message.reply_text(' untuk saat ini, pekerjaanku ' +	random.choice(Job) )
 
-	print('\n====== Apapun yang sudah kamu lakukan hari ini, ' + random.choice(Quotes) )
-	
-output()
+	update.message.reply_text(' Apapun yang sudah kamu lakukan hari ini, ' + random.choice(Quotes))
+
+	update.message.reply_text(' /reset ')
+
+updater.dispatcher.add_handler(CommandHandler('mulai', mulai))
+updater.dispatcher.add_handler(CommandHandler('bantuan', bantuan))
+updater.dispatcher.add_handler(CommandHandler('reset', mulai))
+updater.dispatcher.add_handler(CommandHandler('jalankan', bot))
+updater.dispatcher.add_handler(MessageHandler(Filters.text, unknown))
+updater.dispatcher.add_handler(MessageHandler(Filters.command, unknown))
+
+updater.start_polling()
